0bdb45f68f8b16f36c86b15e93411502f536a766
Chishibau <zshong01@students.cs.ubc.ca>
2025-01-18 16:02:14 -0800

    placeholders for login button and main page complete

--- spotlight/src/App.jsx
+++ spotlight/src/App.jsx
@@ -1,40 +1,34 @@
 import { Route, Routes, useNavigate } from "react-router-dom";
-import Login from "./pages/Login";
+import Main from "./pages/Main";
+import './App.css';
+
 
 const Button = () => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const handleClick = () => {
-    navigate("/login");
-  };
+    const handleClick = () => {
+        navigate("/main");
+    };
 
-  return (
-    <button
-      onClick={handleClick}
-      style={{
-        padding: "10px 20px",
-        fontSize: "16px",
-        cursor: "pointer",
-        outline: "none",
-        border: "2px solid green",
-        borderRadius: "4px",
-        marginTop: "16px",
-        marginBottom: "16px",
-      }}
-    >
-      Go to Login
-    </button>
-  );
+    return (
+        <button
+            onClick={handleClick}
+        >
+            Login
+        </button>
+    );
 };
 
 const App = () => (
-  <div style={{ textAlign: "center" }}>
-    <Button />
-    <Routes>
-      <Route path="/" element={<div>Welcome to the Home page</div>} />
-      <Route path="/login" element={<Login />} />
-    </Routes>
-  </div>
+    <div style={{ textAlign: "center" }}>
+
+        <Routes>
+            <Route path="/" element={<div>
+                <Button />
+                Welcome to Login</div>} />
+            <Route path="/main" element={<Main />} />
+        </Routes>
+    </div>
 );
 
 export default App;
---
+++ spotlight/src/pages/Main.jsx
@@ -0,0 +1,9 @@
+const Main = () => {
+	return (
+        <div>
+            <h1>Main Page Place Holder</h1>
+        </div>
+    );
+};
+
+export default Main;
